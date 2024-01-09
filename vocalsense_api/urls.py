e37cd49cd9f2ccf0fa7031862b9802e185c13cd1
Lejeunesage <geoffroyotegbeye@gmail.com>
2024-01-09 11:49:16 +0100

    Crud keyword disponible

--- vocalsense_api/urls.py
+++ vocalsense_api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views.campaign_views import CampagnViewSet
 from .views.activity_views import ActivityViewSet
-from .views.keyword_views import KeywordList, KeywordDetail
+from .views.keyword_views import KeywordViewSet
 from .views.conversation_views import ConversationList, ConversationDetail
 from .views.message_views import MessageList, MessageDetail
 from .views.statistic_views import StatisticList, StatisticDetail
@@ -13,22 +13,28 @@ router.register(r'campaigns', CampagnViewSet, basename='campaigns')
 
 urlpatterns = [
     # Les URLs pour les opérations CRUD Campagne
-    path('campaigns-list', CampagnViewSet.list, name='campaign-list'),
+    path('campaigns-list', CampagnViewSet.list, name='campaigns-list'),
     path('store-campaign', CampagnViewSet.store_campaign, name='store-campaign'),
     path('update-campaign', CampagnViewSet.update_campaign, name='update-campaign'),
     path('<int:campaign_id>/get-campaign', CampagnViewSet.get_campaign, name='get-campaign-detail'),
     path('<int:campaign_id>/delete-campaign', CampagnViewSet.delete_campaign, name='campaign-delete'),
 
-    # Les URLs pour les opérations CRUD Campagne
-    path('activities-list', ActivityViewSet.list, name='activity-list'),
+    # Les URLs pour les opérations CRUD Activité
+    path('activities-list', ActivityViewSet.list, name='activities-list'),
     path('store-activity', ActivityViewSet.store_activity, name='store-activity'),
     path('update-activity', ActivityViewSet.update_activity, name='update-activity'),
     path('<int:activity_id>/get-activity', ActivityViewSet.get_activity, name='get-activity-detail'),
     path('<int:activity_id>/delete-activity', ActivityViewSet.delete_activity, name='activity-delete'),
+
+    # Les URLs pour les opérations CRUD Mot Clé
+    path('keywords-list', KeywordViewSet.list, name='Keywords-list'),
+    path('store-keyword', KeywordViewSet.store_keyword, name='store-keyword'),
+    path('update-keyword', KeywordViewSet.update_keyword, name='update-keyword'),
+    path('<int:keyword_id>/get-keyword', KeywordViewSet.get_keyword, name='get-keyword-detail'),
+    path('<int:keyword_id>/delete-keyword', KeywordViewSet.delete_keyword, name='keyword-delete'),
     
    
-    path('keywords/', KeywordList.as_view(), name='keyword-list'),
-    path('keywords/<int:pk>/', KeywordDetail.as_view(), name='keyword-detail'),
+    
 
     path('conversations/', ConversationList.as_view(), name='conversation-list'),
     path('conversations/<int:pk>/', ConversationDetail.as_view(), name='conversation-detail'),
--- vocalsense_api/views/keyword_views.py
+++ vocalsense_api/views/keyword_views.py
@@ -1,11 +1,165 @@
 from rest_framework import generics
 from vocalsense_api.models import Keyword
 from vocalsense_api.serializers.keyword_serializers import KeywordSerializer
+from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 
-class KeywordList(generics.ListCreateAPIView):
-    queryset = Keyword.objects.all()
-    serializer_class = KeywordSerializer
 
-class KeywordDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Keyword.objects.all()
-    serializer_class = KeywordSerializer
+class KeywordViewSet(viewsets.ModelViewSet):
+
+    # Méthode pour recupérer la list des mots clés
+    @action(detail=False, methods=['GET'])
+    def list(self):
+        queryset = Keyword.objects.all()
+        serializer = KeywordSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    
+
+    # Fonction permettant de faire  l'ajout d'un ou plusieurs mot(s) clé(s) en base de donnée
+
+    @action(detail=False, methods=['POST'])
+    @csrf_exempt
+    def store_keyword(request):
+        if request.method == 'POST':
+            try:
+                data = JSONParser().parse(request)
+                nom_motcles = data.get('nom_motcles', [])
+                activity_id = data.get('activity_id')
+
+                # Vérifier si les mots clés avec les mêmes noms existent déjà
+                existing_motcles = Keyword.objects.filter(
+                    nom_motcle__in=nom_motcles).values_list('nom_motcle', flat=True)
+
+                duplicate_motcles = set(existing_motcles)
+                if duplicate_motcles:
+                    return JsonResponse({
+                        'errorMessage': f"Les mots clés suivants existent déjà : {', '.join(duplicate_motcles)}"
+                    }, status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
+
+                # Créer de nouveaux mots clés
+                keyword_list = []
+                for nom_motcle in nom_motcles:
+                    keyword = Keyword.objects.create(
+                        nom_motcle=nom_motcle, activity_id=activity_id)
+                    keyword_list.append(keyword)
+
+                # Après avoir enregistré les mots clés, mettre à jour la variable keyword_list
+                queryset = Keyword.objects.all()
+                updated_keyword_list = KeywordSerializer(
+                    queryset, many=True).data
+
+                return JsonResponse({
+                    'successMessage': 'Mots clés enregistrés avec succès',
+                    'keyword_list': updated_keyword_list
+                }, status=status.HTTP_201_CREATED, content_type='application/json')
+
+            except Exception as e:
+                return JsonResponse({
+                    'errorMessage': f'Erreur lors de la création des mots clés : {str(e)}'
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR, content_type='application/json')
+
+        else:
+            return JsonResponse({'errorMessage': 'Méthode non autorisée'}, status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
+        
+
+
+    # Fonction permettant de faire la mise à jour d'un mot clé
+
+    @action(detail=False, methods=['PUT'])
+    @csrf_exempt
+    def update_keyword(request):
+        if request.method == 'PUT':
+            try:
+                data = JSONParser().parse(request)
+                nom_motcle = data.get('nom_motcle')
+                keyword_id = data.get('id')
+
+                # Vérifier si le mot clé existe
+                keyword = Keyword.objects.filter(id=keyword_id).first()
+
+                if not keyword:
+                    return JsonResponse({'errorMessage': 'Le mot clé spécifié n\'existe pas.'}, status=status.HTTP_404_NOT_FOUND, content_type='application/json')
+
+                # Vérifier si une autre activite a déjà le même nom
+                existing_motcle = Keyword.objects.exclude(
+                    id=keyword_id).filter(nom_motcle=nom_motcle).first()
+
+                if existing_motcle:
+                    return JsonResponse({'errorMessage': 'Un mot clé avec ce nom existe déjà.'}, status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
+
+                # Mettre à jour le mot clé
+                keyword.nom_motcle = nom_motcle
+                keyword.save()
+
+                 # Après avoir enregistré le mot clé, mettre à jour la variable keyword_list
+                queryset = Keyword.objects.all()
+                keyword_list = KeywordSerializer(queryset, many=True).data
+
+                return JsonResponse({'successMessage': 'activite mise à jour avec succès',
+                                     
+                                     'keyword_list': keyword_list
+                                     }, status=status.HTTP_200_OK, content_type='application/json')
+
+            except Exception as e:
+                return JsonResponse({'errorMessage': 'Erreur lors de la mise à jour de le mot clé : ' + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR, content_type='application/json')
+
+        else:
+            return JsonResponse({'errorMessage': 'Méthode non autorisée'}, status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
+        
+        
+
+    # Fonction permettant de recupérer le mot clé à mettre à jours
+
+    @action(detail=False, methods=['GET'])
+    @csrf_exempt
+    def get_keyword(request, keyword_id):
+        if request.method == 'GET':
+            try:
+                # Vérifier si le mot clé existe
+                keyword = Keyword.objects.filter(id=keyword_id).first()
+
+                if not keyword:
+                    return JsonResponse({'errorMessage': 'Le mot clé spécifié n\'existe pas.'}, status=status.HTTP_404_NOT_FOUND, content_type='application/json')
+
+                # Récupérer les informations de le mot clé
+                keyword_data = {
+                    'nom_motcle': keyword.nom_motcle,
+                    # Ajoutez d'autres champs au besoin
+                }
+
+                return JsonResponse(keyword_data, status=status.HTTP_200_OK, content_type='application/json')
+
+            except Exception as e:
+                return JsonResponse({'errorMessage': 'Erreur lors de la récupération des informations de le mot clé : ' + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR, content_type='application/json')
+
+        else:
+            return JsonResponse({'errorMessage': 'Méthode non autorisée'}, status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
+
+    # Fonction permettant de faire la suppression d'un mot clé
+
+    @action(detail=True, methods=['DELETE'])
+    @csrf_exempt
+    def delete_keyword(request, keyword_id):
+        if request.method == 'DELETE':
+            try:
+                # Vérifier si le mot clé existe
+                keyword = Keyword.objects.filter(id=keyword_id).first()
+
+                if not keyword:
+                    return JsonResponse({'errorMessage': 'Le mot clé spécifié n\'existe pas.'}, status=status.HTTP_404_NOT_FOUND, content_type='application/json')
+
+                # Supprimer le mot clé
+                keyword.delete()
+
+                return JsonResponse({'successMessage': 'activite supprimée avec succès'}, status=status.HTTP_200_OK, content_type='application/json')
+
+            except Exception as e:
+                return JsonResponse({'errorMessage': 'Erreur lors de la suppression de le mot clé : ' + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR, content_type='application/json')
+        else:
+            return JsonResponse({'errorMessage': 'Méthode non autorisée'}, status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
